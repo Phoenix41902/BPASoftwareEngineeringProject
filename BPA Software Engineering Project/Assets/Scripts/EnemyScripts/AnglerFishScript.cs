a2a838eca8620ef4295b82968a73bbb3bd0d282c
Phoenix41902 <45673274+Phoenix41902@users.noreply.github.com>
2019-12-07 19:19:42 -0500

    worked on the lazer

---
+++ BPA Software Engineering Project/Assets/Scripts/EnemyScripts/AnglerFishScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnglerFishScripe : MonoBehaviour
+{
+    // set up parts of the fish
+    private float health = 30f;
+    private float speed = 3f;
+
+    // animator
+    [SerializeField]
+    private Animator anglerAnim;
+
+    //     
+
+}
