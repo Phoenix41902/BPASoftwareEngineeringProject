b4ee9beb165c0c7ddf6620dc4199e13772c35383
Phoenix41902 <bmdavis419@protonmail.com>
2020-01-09 17:09:04 -0500

    more work

---
+++ BPA Software Engineering Project/Assets/Scripts/Enviorment/Twilight/BossWallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossWallScript : MonoBehaviour
+{
+    public GameObject bossGate;
+    private bool canGo = true;
+
+    // function to be called when the sub hits the load zone
+    private void Start()
+    {
+        bossGate.transform.position = bossGate.transform.position + new Vector3(0, 1000, 0);
+    }
+
+    // on collision
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerSub" && canGo)
+        {
+            bossGate.transform.position = bossGate.transform.position + new Vector3(0, -1000, 0);
+            canGo = false;
+        }
+    }
+}
